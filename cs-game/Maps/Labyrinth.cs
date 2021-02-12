1280f8c3e573f40243f49db5b8011304900d20d8
Adrien <adrien.vaucard@gmail.com>
2021-02-12 12:19:31 +0100

    feat(Create game): Map generation + random pos

---
+++ cs-game/Maps/Labyrinth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_game.Maps
+{
+    class Labyrinth : Map
+    {
+        public int[,] Grid;
+
+        public Labyrinth()
+        {
+            this.Grid = new int[10, 10] {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+            };
+        }
+    }
+}
--- cs-game/Scenes/CreateGame.cs
+++ cs-game/Scenes/CreateGame.cs
@@ -1,4 +1,5 @@
 ﻿using cs_game.Classes;
+using cs_game.Maps;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +11,20 @@ namespace cs_game.Scenes
     class CreateGame
     {
         public string PlayerName;
+        public Labyrinth Map;
 
         public CreateGame()
         {
             Console.Clear();
             Console.WriteLine("Entrez votre nom :");
             this.PlayerName = Console.ReadLine();
+
+            this.Map = new Labyrinth();
+
+            // Place player randomly
+            Random rnd = new Random();
+
+            this.Map.Grid[rnd.Next(0, 10), rnd.Next(0, 10)] = 1;
         }
 
     }
