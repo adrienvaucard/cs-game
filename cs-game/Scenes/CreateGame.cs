7f60f1653762ac42ce787757e878b2eacf245a17
Adrien <adrien.vaucard@gmail.com>
2021-02-12 10:20:26 +0100

    feat(Create Game): Add Scene

---
+++ cs-game/Scenes/CreateGame.cs
@@ -0,0 +1,22 @@
+﻿using cs_game.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_game.Scenes
+{
+    class CreateGame
+    {
+        public string PlayerName;
+
+        public CreateGame()
+        {
+            Console.Clear();
+            Console.WriteLine("Entrez votre nom :");
+            this.PlayerName = Console.ReadLine();
+        }
+
+    }
+}
--- cs-game/Scenes/Home.cs
+++ cs-game/Scenes/Home.cs
@@ -16,77 +16,13 @@ namespace cs_game.Scenes
             // Define Options
             options = new List<Option>
             {
-                new Option("1 - Create new game", () => WriteTemporaryMessage("New Game")),
-                new Option("2 - Load saved game", () =>  WriteTemporaryMessage("Load Game")),
-                new Option("3 - About", () =>  WriteTemporaryMessage("About")),
+                new Option("1 - Create new game", () => new CreateGame()),
+                new Option("2 - Load saved game", () =>  Console.WriteLine("Load Game")),
+                new Option("3 - About", () =>  Console.WriteLine("About")),
                 new Option("4 - Exit", () => Environment.Exit(0)),
             };
 
-            int index = 0;
-
-            WriteMenu(options, options[index]);
-
-            // HANDLE SELECTOR
-            ConsoleKeyInfo keyinfo;
-            do
-            {
-                keyinfo = Console.ReadKey();
-
-                // Handle each key input (down arrow will write the menu again with a different selected item)
-                if (keyinfo.Key == ConsoleKey.DownArrow)
-                {
-                    if (index + 1 < options.Count)
-                    {
-                        index++;
-                        WriteMenu(options, options[index]);
-                    }
-                }
-                if (keyinfo.Key == ConsoleKey.UpArrow)
-                {
-                    if (index - 1 >= 0)
-                    {
-                        index--;
-                        WriteMenu(options, options[index]);
-                    }
-                }
-                // Handle different action for the option
-                if (keyinfo.Key == ConsoleKey.Enter)
-                {
-                    options[index].Selected.Invoke();
-                    index = 0;
-                }
-            }
-            while (keyinfo.Key != ConsoleKey.X);
-
-            Console.ReadKey();
-        }
-        
-        // Default action of all the options. You can create more methods
-        static void WriteTemporaryMessage(string message)
-        {
-            Console.Clear();
-            Console.WriteLine(message);
-            Thread.Sleep(3000);
-            WriteMenu(options, options.First());
-        }
-
-        static void WriteMenu(List<Option> options, Option selectedOption)
-        {
-            Console.Clear();
-
-            foreach (Option option in options)
-            {
-                if (option == selectedOption)
-                {
-                    Console.Write("> ");
-                }
-                else
-                {
-                    Console.Write(" ");
-                }
-
-                Console.WriteLine(option.Name);
-            }
+            var selector = new OptionSelector(options);
         }
     }
 }
